e61f26b87c8bc35e024da7c93262990c97800c67
interdigitize <interdigitize@users.noreply.github.com>
2017-06-27 09:09:30 -0700

    Comments out get and post users requests on the server and adds a simple hello world get request.

--- server/index.js
+++ server/index.js
@@ -8,29 +8,34 @@ app.use(express.static(__dirname + '/../react-client/dist'));
 app.use(bodyParser.json()); // augment the req with body property which will have json from the post's body
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get('/users', function (req, res) {
-  db.selectAll()
-    .then(results => {
-      console.log('these are the results from /users get', results);
-      res.status(200).end(JSON.stringify(results));
-    })
-    .catch(err => {
-      console.error('we have a error ', err);
-      res.status(500).end();
-    });
-});
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
 
-app.post('/users', function (req, res) {
-  db.addUser(req.body)
-    .then(results => {
-      console.log('these are the results from /users post ', results);
-      res.status(201).end();
-    })
-    .catch(err => {
-      console.error('we have a error ', err);
-      res.status(500).end();
-    });
-});
+
+// app.get('/users', function (req, res) {
+//   db.selectAll()
+//     .then(results => {
+//       console.log('these are the results from /users get', results);
+//       res.status(200).end(JSON.stringify(results));
+//     })
+//     .catch(err => {
+//       console.error('we have a error ', err);
+//       res.status(500).end();
+//     });
+// });
+//
+// app.post('/users', function (req, res) {
+//   db.addUser(req.body)
+//     .then(results => {
+//       console.log('these are the results from /users post ', results);
+//       res.status(201).end();
+//     })
+//     .catch(err => {
+//       console.error('we have a error ', err);
+//       res.status(500).end();
+//     });
+// });
 
 app.listen(3000, function() {
   console.log('listening on port 3000!');
